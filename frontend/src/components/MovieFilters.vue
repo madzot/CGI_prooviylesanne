1330de8ce11c571ed677f455fd852c090c8ef111
Mark Dzotsenidze <markdzo@hotmail.com>
2024-03-24 14:16:38 +0200

    Filter component

--- frontend/src/components/MovieFilters.vue
+++ frontend/src/components/MovieFilters.vue
@@ -2,8 +2,89 @@
   <v-layout>
     <v-main>
       <v-container>
-        <v-card>
+        <v-card
+            color="#ededed"
+            max-width="200"
+        >
+          <v-combobox
+              clearable
+              v-model="age"
+              class="pt-4 px-2"
+              density="compact"
+              label="Age restriction"
+              :items="['6+', '13+', '16+', '18+']"
+          >
+          </v-combobox>
+          <v-divider></v-divider>
+          <v-card-item>
+            <v-card-subtitle>
+              Movie genres:
+            </v-card-subtitle>
+            <v-checkbox
+                density="compact"
+                hide-details
+                class="ma-0 pa-0"
+                label="Thriller"
+                v-model="genres"
+                value="thriller"
+            />
+            <v-checkbox
+                density="compact"
+                hide-details
+                label="Comedy"
+                v-model="genres"
+                value="comedy"
+            />
+            <v-checkbox
+                density="compact"
+                hide-details
+                label="Action"
+                v-model="genres"
+                value="action"
+            />
+            <v-checkbox
+                density="compact"
+                hide-details
+                label="Romantic"
+                v-model="genres"
+                value="romantic"
+            />
+            <v-checkbox
+                density="compact"
+                hide-details
+                label="Documentary"
+                v-model="genres"
+                value="documentary"
+            />
+            <v-divider/>
+            <v-card-subtitle
 
+                class="mt-4"
+            >
+              Starting time
+            </v-card-subtitle>
+            <v-range-slider
+                strict
+                step="1"
+                max="24"
+                thumb-label
+                color="blue"
+            >
+            </v-range-slider>
+            <v-divider/>
+            <v-card-subtitle class="mt-4">
+              Language:
+            </v-card-subtitle>
+            <v-combobox
+                density="compact"
+                label="Language"
+                chips
+                multiple
+                v-model="language"
+                :items="['Estonian', 'English']"
+
+            ></v-combobox>
+          </v-card-item>
         </v-card>
       </v-container>
     </v-main>
@@ -12,7 +93,13 @@
 
 <script>
 export default {
-  name: "MovieFilters"
+  name: "MovieFilters",
+  data: () => ({
+    genres: [],
+    age: null,
+    language: null,
+    timeRange: null,
+  })
 }
 </script>
 
